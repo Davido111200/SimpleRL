2e29baa91652912ba6e30f1dca184a63b13c5d97
daidox <davido111200@gmail.com>
2023-07-27 20:28:23 +0700

    REINFORCE: env fixed

--- REINFORCE/short_corridor.py
+++ REINFORCE/short_corridor.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 device = torch.device("cpu")
 
 
-class short_corridor(object):
+class short_env(object):
     def __init__(self, n_states) -> None:
         self.n_states = n_states
         self.grid = np.zeros(shape=(self.n_states, 1), device=device)
@@ -18,35 +18,36 @@ class short_corridor(object):
     def check_current_state(self, current_pos):
         if current_pos < 0:
             current_pos = 0
-            terminated = False
-        elif current_pos >= self.n_states:
+        elif current_pos >= self.n_states :
             current_pos = self.n_states - 1
-            terminated = True
         else:
             current_pos = self.n_states
-            terminated = False
-
         # update the current pos to self.current_pos
         self.current_pos = current_pos
-        return current_pos, terminated
+        return current_pos
+    
 
     def make_a_move(self, action, flag):
         "flag is raised when we are in special state"
         if action == 0:
             if flag:
                 temp = self.current_pos + 1
-                self.current_pos, self.terminated = self.check_current_state(temp)
+                self.current_pos = self.check_current_state(temp)
             else:
                 temp = self.current_pos - 1
-                self.current_pos, self.terminated = self.check_current_state(temp)
+                self.current_pos = self.check_current_state(temp)
         elif action == 1:
             if flag:
                 temp = self.current_pos - 1
-                self.current_pos, self.terminated = self.check_current_state(temp)
+                self.current_pos = self.check_current_state(temp)
             else:
                 temp = self.current_pos + 1
-                self.current_pos, self.terminated = self.check_current_state(temp)
+                self.current_pos = self.check_current_state(temp)
 
+        if self.current_pos == self.n_states - 1:
+            self.terminated = True
+        else: 
+            self.terminated = False
 
         return self.current_pos, self.terminated
 
