579ea006c1819a3296889c6a8b015113dfa2e37d
daidox <davido111200@gmail.com>
2023-08-01 00:09:38 +0700

    AC: main loop done

--- Actor_Critic/main.py
+++ Actor_Critic/main.py
@@ -43,33 +43,38 @@ def main(n_epochs):
     lamb_w = 0.7
     alph_thet = 0.3
     alph_w = 0.3
+    
+    total_rewards = []
 
     torch.autograd.set_detect_anomaly(True)
 
     actor = Actor(env.observation_space.shape[0], 128, env.action_space.n)
     critic = Critic(env.observation_space.shape[0], 128)
 
-    criterion = nn.MSELoss()
-
-    optimizer_actor = optim.SGD(params=actor.parameters(), lr=0.001)
-    optimizer_critic = optim.SGD(params=critic.parameters(), lr=0.001)
-
     for epoch in range(n_epochs):
+        if epoch > 0:
+            print("Epoch: {} - total reward: {}".format(epoch, total_rewards[-1]))
         state, _ = env.reset()
         terminated = False
         z_thet = [torch.zeros_like(param, dtype=torch.float32) for param in actor.parameters()]
         z_w = [torch.zeros_like(param, dtype=torch.float32) for param in critic.parameters()]
         I = 1
+        reward_epoch = 0
         while not terminated:
             state = torch.as_tensor(state, dtype=torch.float32, device=device)
             action, action_probs = actor(state)
             next_state, reward, terminated, truncated, _ = env.step(action.item())
 
+            # save reward for measuring performance
+            reward_epoch += reward
+
             next_state = torch.as_tensor(next_state, dtype=torch.float32, device=device)
             target = reward + critic(next_state)
             estimated_state_val = critic(state)
 
+            # td error
             td_error = target.detach() - estimated_state_val
+
 
             ## trace w - update
             loss_w = (target.detach() - estimated_state_val)
@@ -77,13 +82,13 @@ def main(n_epochs):
             loss_w.backward()
 
             # calculate and save the gradients
-            gradients = []
+            gradients_w = []
             for param in critic.parameters():
-                gradients.append(param.grad.data.clone())
+                gradients_w.append(param.grad.data.clone())
 
             # updating z_w
             z_w = [GAMMA * alph_w * i for i in z_w]
-            z_w += gradients
+            z_w += gradients_w
 
             # updating w
             z_w_updates = [alph_w * td_error.item() * i for i in z_w]
@@ -93,30 +98,43 @@ def main(n_epochs):
                 for idx, p in enumerate(critic.parameters()):
                     p.copy_(w_new_params[idx])
             
-            quit()
-            # trace theta - update
-            action_log_probs = torch.log(action_probs).requires_grad()
-            print(action_log_probs)
-            print(target)
-            gradients_theta = - target.detach() * action_log_probs
-            print(gradients_theta)
-            quit()
             
+            ## trace theta - update
+            log_action_probs = torch.log(action_probs[action])
             
+            loss_thet = - (reward * log_action_probs) 
 
+            actor.zero_grad()
+            loss_thet.backward()
 
-            
+            # calculate and save the gradients
+            gradients_thet = []
+            for param in actor.parameters():
+                gradients_thet.append(param.grad.data.clone())
 
+            # updating z_thet 
+            z_thet = [GAMMA * alph_thet * i for i in z_thet]
+            z_thet += [I * i for i in gradients_thet]
 
+            # updating theta
+            z_thet_updates =  [alph_thet * td_error.item() * i for i in z_thet]
+            assert len(z_thet_updates) == len(z_thet)
+            thet_new_params = list(actor.parameters()) + z_thet_updates
+            with torch.no_grad():
+                for idx, p in enumerate(actor.parameters()):
+                    p.copy_(thet_new_params[idx]) 
+                       
+            I *= GAMMA
+            state = next_state  
 
-
+        total_rewards.append(reward_epoch)
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--n_epochs', type=int, default=1)
+    parser.add_argument('--n_epochs', type=int, default=100)
 
     args = parser.parse_args()
     main(args.n_epochs)
