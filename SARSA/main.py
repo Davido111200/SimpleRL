384c7f80f5102fe81d992b170c8bc8d381661521
Davido111200 <davido111200@gmail.com>
2023-08-15 12:19:16 +1000

    continue sarsa

--- SARSA/main.py
+++ SARSA/main.py
@@ -27,8 +27,29 @@ class neural_net(nn.Module):
         x = F.relu(self.fc1(x))
         return torch.argmax(F.softmax(self.fc2(x))), F.softmax(self.fc2(x))
     
+
 def main(n_epochs, max_ts):
     env = gym.make('CartPole-v1')
+    net = neural_net(env.observation_space.shape[0], env.action_space.n, device=device)
+
+    def action_selection(eps, state):
+        temp = random.random()
+        if temp < eps:
+            return torch.randint(low=0, high=env.action_space.n - 1)
+        else:
+            state_tensor = net.encode(state)
+            return net(state_tensor)[0]
+        
+    def anneal_eps():
+        pass
+
+
+    for epoch in range(n_epochs):
+        state, _ = env.reset()
+        terminated = False
+
+        for ts in range(max_ts):
+            action = action_selection()
 
 
 if __name__ == "__main__":
