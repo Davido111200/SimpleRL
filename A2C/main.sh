829eef7ba90a87aa7e30402b3cf481c64145f6d1
Davido111200 <davido111200@gmail.com>
2023-09-02 20:00:37 +1000

    wandb update

---
+++ A2C/main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH -J interact
+#SBATCH -o output.log
+#SBATCH -e error.log          
+#SBATCH -n 1
+#SBATCH --gres=gpu:2
+
+cd ~/dai/SimpleRL/A2C
+
+n_epochs=2000
+max_ts=1000
+n_trials=5
+
+python3 main.py --n_epochs "$n_epochs" --max_ts "$max_ts" --n_trials "$n_trials"
