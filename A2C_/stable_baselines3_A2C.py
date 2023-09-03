0497b52e8eebf57e137e08ba15332774d08c7066
Davido111200 <davido111200@gmail.com>
2023-09-03 16:23:26 +1000

    A2C - stablebaselines3 agent added

--- A2C_/stable_baselines3_A2C.py
+++ A2C_/stable_baselines3_A2C.py
@@ -1,20 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from stable_baselines3 import A2C
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments
-vec_env = make_vec_env("CartPole-v1", n_envs=4)
+env = make_vec_env("CartPole-v1", n_envs=1)
 
-model = A2C("MlpPolicy", vec_env, verbose=1)
-model.learn(total_timesteps=25000)
-model.save("a2c_cartpole")
+# Create and train the A2C agent
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
 
-del model # remove to demonstrate saving and loading
+# Collect the episode rewards during training
+episode_rewards = []
+episodes = 1000  # Number of episodes to run for data collection
 
-model = A2C.load("a2c_cartpole")
+for _ in range(episodes):
+    obs = env.reset()
+    total_reward = 0
 
-obs = vec_env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = vec_env.step(action)
-    vec_env.render("human")
-print("Done")
+    while True:
+        action, _ = model.predict(obs)
+        obs, reward, done, _ = env.step(action)
+        total_reward += reward
+
+        if done:
+            episode_rewards.append(total_reward)
+            break
+
+# Plot the training rewards
+episode_numbers = np.arange(1, len(episode_rewards) + 1)
+
+plt.plot(episode_numbers, episode_rewards)
+plt.xlabel('Episode Number')
+plt.ylabel('Episode Reward')
+plt.title('Training Reward over Episodes')
+
+# Save the plot as an image file (e.g., PNG)
+plt.savefig('/home/s223540177/dai/SimpleRL/A2C_/figs/sb3_training_rewards_plot.png')
+
+# Show the plot (optional)
+plt.show()
